387d1279a5002d5704615893b23c688c36e6cfaf
alexqmv <96629106+alexqmv@users.noreply.github.com>
2022-07-04 19:12:53 +0400

    [upd] - code optimising

--- hashServ.py
+++ hashServ.py
@@ -1,4 +1,4 @@
-import hashlib #importing hashlib library
+import hashlib
 import time
 import smtplib
 import random
@@ -6,27 +6,37 @@ from os.path import basename
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
-email_sender = "YOURMAIL.RU"
-password = "YOUR MAIL.RU PASSWORD"
- 
 
-smtp_server = smtplib.SMTP("smtp.mail.ru", 2525)
-smtp_server.starttls()
+email_sender = "" # Enter your mail
+password = ""# Enter your password
+
+smtp_server = smtplib.SMTP("smtp.mail.ru", 2525) # Connected to smtp Mail.Ru server,with port 2525, you can also try 25,465,467,587 - for Gmail
+smtp_server.starttls() # Starting
+id = 4357 # Under maintance | I wanted to create random generation of code verification, maybe  i will do it soon
+#id = random.randint(1000,10000)
 
 msg = MIMEMultipart()
-msg.attach(MIMEText("Hi, dear user!\n\nYou chose this mail for verification? Was that you?"))
+msg.attach(MIMEText("Your verification code \n\t\t\t\t\t4357 \n\n\n\nReply -> \n@help.feedback@mail.ru\n\n\n\n\t\t\t\t\t\t(C) StarEye Blockchain"))
+# Just messages
 
+
+# Creating hashGen main class
 
 class Networkus_Blockchain: # Main Class
 
-	def __init__(self, previous_block_hash,transaction_list): #initiliazing
+
+	def __init__(self,previous_block_hash,transaction_list): #initiliazing
+
 		self.previous_block_hash = previous_block_hash #block hash
 		self.transaction_list = transaction_list #transaction list
 
 		self.block_data = "-".join(transaction_list) + "-" + previous_block_hash #random hash symbols
-		self.block_hash = hashlib.md5(self.block_data.encode()).hexdigest()
+		self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
 
-auth = str(input("[!] - Verify your identity by @mail  (Enter your valid mail)  - "))
+
+
+auth = str(input("[!] - Verify your identity by Mail (Enter your valid mail)  - "))
+
 email_getter = auth
 
 
@@ -35,7 +45,7 @@ html2 = """\
 <html>
  <head></head>
  <body>
-   <a href="https://dribbble.com/shots/15750262-Success-Message-Verification">No,it wasn't me!</a>
+   <a href="https://dribbble.com/shots/15750262-Success-Message-Verification">License</a>
 </html>
 """
 
@@ -43,7 +53,7 @@ html = """\
 <html>
  <head></head>
  <body>
-   <a href="https://dribbble.com/shots/15750262-Success-Message-Verification">Verify Account NOW.</a>
+   <a href="https://dribbble.com/shots/15750262-Success-Message-Verification">Secure my hash (SOON)</a>
 </html>
 """
 
@@ -52,16 +62,43 @@ t1 = str(input("Enter string to generate hash: "))
 print("~----------------HASH GENERATOR----------------~")
 
 
-initial_block = Networkus_Blockchain(" NetworkusChain", [t1]) #initial block where we use transaction1 and transaction2
+initial_block = Networkus_Blockchain(" StarEye Blockchain", [t1]) #initial block where we use transaction1 and transaction2
 print(initial_block.block_data) # just printing this block
-print("Generated hash login ->> ", initial_block.block_hash)
+print("Generated hash > ", initial_block.block_hash)
 print("-----------------------------------------------")
+
 
 
 msg.attach(MIMEText(html2, "html"))
 
-msg.attach(MIMEText("\n\nIf it's you, let's verify account by tapping here => "))
+msg.attach(MIMEText("\n\nJOIN TO STAREYE DEVELOPERS"))
 msg.attach(MIMEText(html, "html"))
+
+# SENDING FILES TO MAIL
+with open("readME.txt", "rb") as f:
+    file = MIMEApplication(f.read(), Name=basename("readME.txt"))
+msg.attach(file)
+
+with open("ver.png", "rb") as f:
+    file = MIMEApplication(f.read(), Name=basename("verify.png"))
+msg.attach(file)
 
 smtp_server.login(email_sender, password)
 smtp_server.sendmail(email_sender, email_getter, msg.as_string())
+
+
+# CODE CHECKING
+# IF code not equal to id(4357), we give request to do it again.
+# THIS CODE YOU CAN FIND IN YOUR MAIL, BUT IT IS UNDER DEVELOPMENT!!
+# THERE IS ONLY ONE CODE YET. SOON IT WILL BE SENDING RANDOMLY.!!
+# IF YOU WANT TO UPGRADE MY CODE  JUST FIND ME ON INSTAGRAM @alex.qmv
+# ENJOY
+while (True):
+	print("Check your mail.")
+	code = int(input("Enter verification code from your mail: "))
+	if (code != id):
+		print("[E:503 - verification code error!]")
+
+	else:
+		print("Successfully")
+		break
